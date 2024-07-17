ef3c4f291cab49835bbdf365a4ac81729a672936
poppuppy <91822204+poppuppy@users.noreply.github.com>
2024-07-17 19:17:34 +0800

    Add files via upload

---
+++ Next-DiT-MoE/exps/slurm/3B_bs256_lr5e-4_bf16_qknorm_lognorm.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+train_data_root='/path/to/imagenet/images/train'
+
+model=DiT_Llama_3B_patch2
+batch_size=256
+lr=5e-4
+precision=bf16
+
+exp_name=${model}_bs${batch_size}_lr${lr}_${precision}_qknorm
+mkdir -p results/"$exp_name"
+
+python -u train.py \
+    --model ${model} \
+    --data_path ${train_data_root} \
+    --results_dir results/"$exp_name" \
+    --micro_batch_size 32 \
+    --global_batch_size ${batch_size} --lr ${lr} \
+    --data_parallel sdp \
+    --max_steps 3000000 \
+    --ckpt_every 10000 --log_every 100 \
+    --precision ${precision} --grad_precision fp32 --qk_norm \
+    --snr_type "lognorm" \
+    2>&1 | tee -a results/"$exp_name"/output.log
---
+++ Next-DiT-MoE/scripts/run_8gpus.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# run Next-DiT with single node
+
+# run Next-DiT 600M
+bash exps/600M_bs256_lr5e-4_bf16_qknorm_lognorm.sh
